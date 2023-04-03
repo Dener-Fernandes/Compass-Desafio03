4e26a5aebe77cfb7e4bf1ff313b02d7ee0ebedf2
Dener-Fernandes <dener.fernandes.oliveira@gmail.com>
2023-04-03 14:39:58 -0400

    feat: add database connection

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import { connect } from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config({ path: "./config.env" });
+
 app.use(express.json());
+
+const dataBase = process.env.DATABASE!.replace("<PASSWORD>", process.env.DATABASE_PASSWORD!);
+connect(dataBase).then(() => console.log("DB connection successful!")).catch((err)=> {
+  console.log("Could not connect to database");
+});
 
 export { app }
