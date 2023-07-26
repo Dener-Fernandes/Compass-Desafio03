4db57199eb060164601fff6c5fe6dd6d03d6c5c8
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2023-07-26 14:36:09 -0400

    test: add unit testing for MongoDBCarRepository class

--- src/tests/MongoDBCarRepository.spec.ts
+++ src/tests/MongoDBCarRepository.spec.ts
@@ -1,61 +1,171 @@
 import { MongoDBCarRepository } from "../repositories/MongoDBCarRepository";
 import { Car } from "../models/Car";
+import {
+  ICarRepository,
+  PaginationOption01,
+  PaginationOption02,
+} from "../interfaces";
+import { ICar } from "../interfaces/ICar";
+import { verifyQueryValues } from "../utils/verifyQueryValues";
 
-const repository = new MongoDBCarRepository(Car);
+let carsRepository: ICarRepository;
 
-const carExpected = {
-  _id: "1221",
-  model: "Civic",
-  color: "Red",
-  year: "2000",
-  value_per_day: 125.52,
-  accessories: [
-    {
-      description: "É um carro",
-    },
-  ],
-  number_of_passengers: 4,
-};
-const carExpectedUpdate = {
-  _id: "123",
-  model: "Ford Cortina",
-  color: "Green",
-  year: "1974",
-  value_per_day: 125.52,
-  accessories: [
-    {
-      description: "É um carro",
-    },
-  ],
-  number_of_passengers: 4,
-};
+interface ICarTest extends ICar {
+  toObject?(): ICarTest | ICarTest[];
+}
 
-const valueReturnedWhenDeletedIsSuccessful = 1;
+interface ICarsExpectedList {
+  values: ICarTest[];
+  toObject?(): ICarTest | ICarTest[];
+  sort(query: { _id: string }): ICarsExpectedList;
+  skip(offset: PaginationOption02): ICarsExpectedList;
+  limit(limit: PaginationOption02): ICarTest[];
+}
 
 describe("carRepository's unit test", () => {
-  beforeAll(() => {
-    repository.create = jest.fn().mockReturnValueOnce(carExpected);
-    repository.getById = jest.fn().mockReturnValueOnce(carExpected);
-    repository.update = jest.fn().mockReturnValueOnce(carExpectedUpdate);
-    repository.delete = jest
-      .fn()
-      .mockReturnValueOnce(valueReturnedWhenDeletedIsSuccessful);
+  beforeEach(async () => {
+    const carsExpected: ICarTest[] = [
+      {
+        _id: "64becfc13c54e17e41e4b301",
+        model: "Opala",
+        color: "Red",
+        year: "1974",
+        value_per_day: 125.52,
+        accessories: [
+          {
+            description: "It is a red car",
+          },
+        ],
+        number_of_passengers: 4,
+        toObject: (): ICarTest => {
+          return carsExpected[0];
+        },
+      },
+      {
+        _id: "64becf6e3c54e17e41e4b2f8",
+        model: "Ford Cortina",
+        color: "Green",
+        year: "1974",
+        value_per_day: 125.52,
+        accessories: [
+          {
+            description: "It is a car",
+          },
+        ],
+        number_of_passengers: 4,
+        toObject: (): ICarTest => {
+          return carsExpected[1];
+        },
+      },
+      {
+        _id: "64becf953c54e17e41e4b2fb",
+        model: "Corola",
+        color: "Black",
+        year: "2008",
+        value_per_day: 125.52,
+        accessories: [
+          {
+            description: "It is a night car",
+          },
+        ],
+        number_of_passengers: 4,
+        toObject: (): ICarTest => {
+          return carsExpected[0];
+        },
+      },
+      {
+        _id: "64becfad3c54e17e41e4b2fe",
+        model: "Saveiro",
+        color: "Red",
+        year: "2012",
+        value_per_day: 125.52,
+        accessories: [
+          {
+            description: "It is a day car",
+          },
+        ],
+        number_of_passengers: 4,
+        toObject: (): ICarTest => {
+          return carsExpected[0];
+        },
+      },
+      {
+        model: "Corvette",
+        color: "Dark Blue",
+        year: "2020",
+        value_per_day: 125.52,
+        accessories: [
+          {
+            description: "It is a great car",
+          },
+        ],
+        number_of_passengers: 4,
+        _id: "64c114b896a4157e1adc7745",
+        toObject: (): ICarTest => {
+          return carsExpected[0];
+        },
+      },
+    ];
+
+    const carsExpectedList: ICarsExpectedList = {
+      values: carsExpected,
+      toObject: (): ICarTest[] => {
+        // This method should emulate mongoose's toObject method. It is and adapted method.
+        return carsExpectedList.values;
+      },
+      sort(query: { _id: string }): ICarsExpectedList {
+        // This method should emulate mongoose's sort method. It is and adapted method.
+        return carsExpectedList;
+      },
+      skip(offset): ICarsExpectedList {
+        // This method should emulate mongoose's skip method. The reverse (sort) is
+        // done here, and not on the previous method, to avoid using it twice. It is and adapted method.
+        this.values = this.values.slice(offset).reverse();
+        return carsExpectedList;
+      },
+      limit: function (limit: number): ICarTest[] {
+        // This method should emulate mongoose's limit method. It is and adapted method.
+        if (limit) {
+          let newValues: ICarTest[] = [];
+
+          for (let i = 0; i < limit; i++) {
+            newValues.push(this.values[i]);
+          }
+
+          return (this.values = newValues);
+        } else {
+          return this.values;
+        }
+      },
+    };
+
+    const deletedResult = { acknowledged: true, deletedCount: 1 };
+    const documentsQuantity = carsExpected.length;
+
+    Car.create = jest.fn().mockReturnValueOnce(carsExpected[0]);
+    Car.findById = jest.fn().mockReturnValueOnce(carsExpected[0]);
+    Car.find = jest.fn().mockReturnValueOnce(carsExpectedList);
+    Car.findByIdAndUpdate = jest.fn().mockReturnValueOnce(carsExpected[1]);
+    Car.countDocuments = jest.fn().mockReturnValueOnce(documentsQuantity);
+    Car.deleteOne = jest.fn().mockReturnValueOnce(deletedResult);
+
+    carsRepository = new MongoDBCarRepository(Car);
   });
 
   it("should be able to create a new car", async () => {
     const car = {
-      model: "Civic",
+      model: "Opala",
       color: "Red",
-      year: "2000",
+      year: "1974",
       value_per_day: 125.52,
       accessories: [
         {
-          description: "É um carro",
+          description: "It is a red car",
         },
       ],
       number_of_passengers: 4,
     };
-    const result = await repository.create(car);
+    const result = await carsRepository.create(car);
 
     expect(result.model).toBe(car.model);
     expect(result.color).toBe(car.color);
@@ -67,49 +177,128 @@ describe("carRepository's unit test", () => {
   });
 
   it("should be able to find a car by id", async () => {
-    const car = {
-      _id: "1221",
-      model: "Civic",
-      color: "Red",
-      year: "2000",
-      value_per_day: 125.52,
-      accessories: [
-        {
-          description: "É um carro",
-        },
-      ],
-      number_of_passengers: 4,
-    };
+    const carId = "64becfc13c54e17e41e4b301";
+    const result = await carsRepository.getById(carId);
 
-    const result = await repository.getById("1221");
+    expect(result?._id).toBe(carId);
+  });
 
-    expect(result?._id).toBe(car._id);
+  it("should be able to list all the cars (with query params)", async () => {
+    const query: PaginationOption01 = [
+      { color: "Black" },
+      { model: "Saveiro" },
+      { "accessories.description": "It is a great car" },
+    ];
+
+    const limit = 3;
+    const offset = 2;
+
+    const result = await carsRepository.listAll(query, offset, limit);
+
+    expect(result.length).toBe(limit);
+  });
+
+  it("should be able to list all the cars (with no query params)", async () => {
+    const query: PaginationOption01 = [];
+
+    const limit = 3;
+    const offset = 2;
+
+    const result = await carsRepository.listAll(query, offset, limit);
+
+    expect(result.length).toBe(limit);
   });
 
   it("should be able to update a car by id", async () => {
     const car = {
+      _id: "64becf6e3c54e17e41e4b2f8",
       model: "Ford Cortina",
-      color: "Red",
+      color: "Green",
       year: "1974",
       value_per_day: 125.52,
       accessories: [
         {
-          description: "É um carro",
+          description: "It is a car",
         },
       ],
       number_of_passengers: 4,
     };
-    const result = await repository.update("123", car);
+    const carId = "64becf6e3c54e17e41e4b2f8";
+
+    const result = await carsRepository.update(carId, car);
 
     expect(result?.model).toBe(car.model);
     expect(result?.year).toBe(car.year);
   });
 
-  it("should be able to delete a car by id", async () => {
-    const carId = "123";
+  it("should be able to return the quantity of documents", async () => {
+    const result = await carsRepository.countItems();
 
-    const result = await repository.delete(carId);
+    expect(result).toBe(5);
+  });
+
+  it("should be able to delete a car by id", async () => {
+    const carId = "64becfc13c54e17e41e4b301";
+
+    const result = await carsRepository.delete(carId);
 
     expect(result).toBe(1);
+  });
+
+  it("should not be able to find a car by id", async () => {
+    const carId = "64becfc13c54e17e41e4b301";
+
+    Car.findById = jest.fn().mockReturnValueOnce(undefined);
+
+    const localCarRepository = new MongoDBCarRepository(Car);
+
+    const result = await localCarRepository.getById(carId);
+
+    expect(result).toBe(undefined);
+  });
+
+  it("should not be able to update a car by id", async () => {
+    const car = {
+      _id: "64becf6e3c54e17e41e4b2f8",
+      model: "Ford Cortina",
+      color: "Green",
+      year: "1974",
+      value_per_day: 125.52,
+      accessories: [
+        {
+          description: "It is a car",
+        },
+      ],
+      number_of_passengers: 4,
+    };
+    const carId = "64becf6e3c54e17e41e4b2f8";
+
+    Car.findByIdAndUpdate = jest.fn().mockReturnValueOnce(undefined);
+
+    const localCarRepository = new MongoDBCarRepository(Car);
+
+    const result = await localCarRepository.update(carId, car);
+
+    expect(result).toBe(undefined);
+  });
+
+  it("should be able to return true when finding a query param", async () => {
+    const query: PaginationOption01 = [
+      { color: "Black" },
+      { model: "Saveiro" },
+      { "accessories.description": "It is a great car" },
+    ];
+
+    const result = await verifyQueryValues(query);
+
+    expect(result).toBe(true);
+  });
+
+  it("should be able to return false when not finding any query param", async () => {
+    const query: PaginationOption01 = [];
+
+    const result = await verifyQueryValues(query);
+
+    expect(result).toBe(false);
   });
 });
